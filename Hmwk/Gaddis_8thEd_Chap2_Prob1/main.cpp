58c3104051746865255144eb0840bfb1aae64611
jl2537358 <jose.lopzandrade@gmail.com>
2015-01-08 11:57:21 -0800

    Signed-off-by: jl2537358 <jose.lopzandrade@gmail.com>

---
+++ Hmwk/Gaddis_8thEd_Chap2_Prob1/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose Lopez
+ * Purpose: To store two numbers and show total
+ * Created on January 8, 2015, 11:38 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Working Prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+    //Declare variables
+    int x=50;
+    int y=100;
+    //Calculate the problem
+    short total=x+y;
+    //Output the results
+    cout<<"The variables x stands for 50 and the variable y stands"<<endl;
+    cout<<" for 100. The sum of these variables is as follows."<<endl;
+    cout<<"     x + y = "<<total<<endl;
+    return 0;
+}
+
