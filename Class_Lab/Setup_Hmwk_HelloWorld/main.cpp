984e4ecf1f1aebb3322a6228ab1d9cb390564205
jl2537358 <jose.lopezandrade@gmail.com>
2015-01-07 23:03:57 -0800

    Signed-off-by: jl2537358 <jose.lopezandrade@gmail.com>

---
+++ Class_Lab/Setup_Hmwk_HelloWorld/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   Setup_Hmwk_Helloworld
+ * Author: Jose Lopez
+ * Created on January 7, 2015, 10:54 PM
+ * Purpose: For setup of Github account thingy.
+ */
+
+//System Library
+#include <iostream>
+using namespace std;
+
+//User Library
+
+//Global Constant
+
+//Functioning Prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+    cout<<"Hello World"<<endl;
+    return 0;
+}
+
---
+++ Class_Lab/Setup_Hmwk_HelloWorld/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Debug=setup_hmwk_helloworld
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/setup_hmwk_helloworld
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Debug=setuphmwkhelloworld.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/setuphmwkhelloworld.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Release=setup_hmwk_helloworld
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/setup_hmwk_helloworld
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Release=setuphmwkhelloworld.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/setuphmwkhelloworld.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
