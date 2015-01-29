18c932a6d06cef050f573a2bdc5182d9e8e34214
jl2537358 <jose.lopezandrade@gmail.com>
2015-01-29 15:53:10 -0800

    Signed-off-by: jl2537358 <jose.lopezandrade@gmail.com>

--- Project1/CppApplication_1/main.cpp
+++ Project1/CppApplication_1/main.cpp
@@ -4,78 +4,94 @@
     #include <cmath>
     using namespace std;
     int main ()
-    {
-    srand((unsigned)time(0));
-    cout << "Welcome to Jose's Blackjack Game! \n";
-    int wager;
-    cout << "\n" << "Please place your wager: $";
-    cin >> wager;	//wager
-    int dealer_card1 = rand() % 13 + 1; //dealer card 1
-    int dealer_card2 = rand() % 13 + 1;	//dealer card 2
-    int player_card1 = rand() % 13 + 1;	//player card 1
-    int player_card2 = rand() % 13 + 1; //player card 2
-    cout << "\n" << "The dealer has ";
-    switch (dealer_card1) {
-    case 1: cout << "Ace and ";
+    {srand((unsigned)time(0));
+    cout<<"Welcome to Jose's Blackpick Game!"<<endl;
+    int nmbrW;
+    cout<<endl;
+    cout<<"Please place your number wanted: ";
+    cin>>nmbrW; //enter wager willing to bet
+    int dealer_card1=rand()%13+ 1; //dealer card 1
+    int dealer_card2=rand()%13+1;  //dealer card 2
+    int player_card1=rand()%13+1;  //player card 1
+    int player_card2=rand()%13+1;  //player card 2
+    cout<<endl;
+    cout<<"The dealer has ";
+    switch(dealer_card1){
+          case 1: 
+              cout<<"an Ace and ";
+        break;
+          case 11: 
+              cout<<"a Jack and ";
+        break;
+           case 12: 
+              cout<<"a Queen and ";
+        break;
+          case 13: 
+              cout<<"a King and ";
+        break;
+        default:cout<<dealer_card1<<" and ";
     break;
-    case 11: cout << "Jack and ";
+    }switch(dealer_card2){
+          case 1: 
+              cout<<"an Ace";
+        break;
+          case 11: 
+              cout<<"a Jack";
+        break;
+          case 12: 
+              cout<<"a Queen";
+        break;
+          case 13: 
+              cout<<"a King";
+        break;
+        default:cout<<dealer_card2;
     break;
-    case 12: cout << "Queen and ";
+    }cout<<endl;
+    cout<<endl;
+    cout<<"You have ";
+    switch(player_card1){
+          case 1: 
+              cout<<"an Ace and ";
+        break;
+          case 11: 
+              cout<<"a Jack and ";
+        break;
+           case 12: 
+              cout<<"a Queen and ";
+        break;
+          case 13: 
+              cout<<"a King and ";
+        break;
+        default:cout<<player_card1<<" and ";
     break;
-    case 13: cout << "King and ";
+    }switch(player_card2){
+          case 1: 
+              cout<<"an Ace";
+        break;
+          case 11: 
+              cout<<"a Jack";
+        break;
+          case 12: 
+              cout<<"a Queen";
+        break;
+          case 13: 
+              cout<<"a King";
+        break;
+        default:cout<<player_card2;
     break;
-    default: cout << dealer_card1 << " and ";
-    break;
+    }cout<<endl;
+    int dealer_total=dealer_card1+dealer_card2;
+    int player_total=player_card1+player_card2;
+    if ((player_card1==1)&&(player_card2==10|11|12|13)){
+    cout<<"Blackjack! You Win $ "<<nmbrW*1.5<<"!!"<<endl;
+    }else if((player_card2==1)&&(player_card1==10|11|12|13)){
+    cout<<"Blackjack! You Win $ "<<nmbrW*1.5 << "!!"<<endl;
+    }else if(player_total>dealer_total){
+    cout<<endl;
+    cout<<"You Win $"<<nmbrW<<"!!"<<endl;
+    }else{
+    cout<<endl;
+    cout<<"You lose"<<endl;
     }
-    switch (dealer_card2) {
-    case 1: cout << "Ace";
-    break;
-    case 11: cout << "Jack";
-    break;
-    case 12: cout << "Queen";
-    break;
-    case 13: cout << "King";
-    break;
-    default: cout << dealer_card2;
-    break;
-    }
-    cout << "\n" << "\n" << "You have ";
-    switch (player_card1){
-    case 1: cout << "Ace and ";
-    break;
-    case 11: cout << "Jack and ";
-    break;
-    case 12: cout << "Queen and ";
-    break;
-    case 13: cout << "King and ";
-    break;
-    default: cout << player_card1 << " and ";
-    break;
-    }
-    switch (player_card2){
-    case 1: cout << "Ace";
-    break;
-    case 11: cout << "Jack";
-    break;
-    case 12: cout << "Queen";
-    break;
-    case 13: cout << "King";
-    break;
-    default: cout << player_card2;
-    break;
-    }
-    cout << "\n";
-    int dealer_total = dealer_card1 + dealer_card2;
-    int player_total = player_card1 + player_card2;
-    if ((player_card1 == 1) && (player_card2 == 10|11|12|13))
-    cout << "Blackjack! You Win $ " << wager*1.5 << "!!" << endl;
-    else
-    if ((player_card2 == 1) && (player_card1 == 10|11|12|13))
-    cout << "Blackjack! You Win $ " << wager*1.5 << "!!" << endl;
-    else
-    if (player_total > dealer_total)
-    cout << "\n" << "You Win $" << wager << "!!" << endl;
-    else
-    cout << "\n" << "You lose" << endl;	
     return 0;
-    } //main
+    }
--- Project1/LopezJose_Gamethingy/main.cpp
+++ Project1/LopezJose_Gamethingy/main.cpp
@@ -1,34 +1,95 @@
 /*
 * File: main.cpp
 * Author: Jose Lopez
+* Purpose: Make a simple game
 * Created on January 29, 2015, 8:56 AM
 */
 //System Libraries
 #include <cstdlib>
 #include <iostream>
+#include <ctime>
+#include <cmath>
 using namespace std;
 //User Libraries
 //Global Constants
 //Function Prototype
 //Execution begins here!
 int main(int argc, char** argv) {
+srand((unsigned)time(0));
 //Declare variables
-char thPick=11;
+int thPick=11;
 bool computer;
-short nPckRmv;
+int nPckRmv;
 //Playing the game
+cout<<"Welcome to Jose's Blackpick Game!"<<endl;
 do{
 //Designate the player
 computer=false;
 //Ask player how many tooth picks to remove
-do{
-cout<<"There are "<<static_cast<int>(thPick)<<" left to remove"<<endl;
-cout<<"How many tooth picks do you want to remove"<<endl;
-cout<<"Choose 1,2, or 3"<<endl;
-cin>>nPckRmv;
+do{ cout<<endl;
+    cout<<"There are "<<static_cast<int>(thPick)<<" left to remove."<<endl;
+    cout<<"Please place your number wanted: "<<endl;
+    cout<<"If the following cards are near enough to 21, your"<<endl;
+    cout<<"desired number will be chosen."<<endl;
+    cin>>nPckRmv; //enter number desired
+    int player_card1=rand()%13+1;  //player card 1
+    int player_card2=rand()%13+1;  //player card 2
+    cout<<endl;
+    cout<<"You got ";
+    switch(player_card1){
+          case 1: 
+              cout<<"an Ace and ";
+        break;
+          case 11: 
+              cout<<"a Jack and ";
+        break;
+           case 12: 
+              cout<<"a Queen and ";
+        break;
+          case 13: 
+              cout<<"a King and ";
+        break;
+        default:cout<<player_card1<<" and ";
+    break;
+    }switch(player_card2){
+          case 1: 
+              cout<<"an Ace";
+        break;
+          case 11: 
+              cout<<"a Jack";
+        break;
+          case 12: 
+              cout<<"a Queen";
+        break;
+          case 13: 
+              cout<<"a King";
+        break;
+        default:cout<<player_card2;
+    break;
+    }cout<<endl;
+        int player_total=player_card1+player_card2;
+        if((player_card1==1)&&(player_card2==10|11|12|13)){
+        cout<<"You got 21!!Your desired number will be used."<<endl;
+    break;
+        }else if(thPick==1){
+        nPckRmv=1;
+    break;
+        }else if((player_card1==2)&&(player_card2==10|11|12|13)){
+        cout<<"You got 21!!Your desired number will be used."<<endl;
+    break;
+        }else if((player_total>18)){
+        cout<<"You got near enough to 21. Your desired number will be used."<<endl;
+    break;
+        }else{
+        cout<<"You weren't near enough to 21. Your number will be"<<endl;
+        cout<<"chosen randomly."<<endl;
+        nPckRmv=rand()%3+1;//Randomly chosen number
+        }
+        cout<<endl;
+        cout<<"Your number chosen was "<<nPckRmv<<"."<<endl;
 }while(nPckRmv<=0||nPckRmv>=4||nPckRmv>thPick);
 //Remove the number of toothpicks
-thPick-=nPckRmv;
+thPick=thPick-nPckRmv;
 //Designate the computer
 computer=true;
 //Utilize logic
@@ -56,14 +117,15 @@ thPick=1;
 thPick=0;
 computer=false;
 }
-}while(thPick>1);
+}while(thPick>=1);
 //Output the winner and loser
 switch(static_cast<int>(computer)){
 case 1:{
 cout<<"Computer wins you lose"<<endl;
 break;
-}default:
+}default:{
 cout<<"Computer loses we win"<<endl;
 }//Exit the Game
+}
 return 0;
 }
